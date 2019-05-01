5f275c3e8b9b126562e6db69ddd1f48f14877672
unknown <miroslavadmiral@gmail.com>
2019-05-01 23:18:45 +0300

    Modificate cart function

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var config = {
   firebase.initializeApp(config);
 // GLOBAL
 var products=[];
-var carItems=[];
+var cartItems=[];
 var cart_n = document.getElementById('cart_n');
 // DIVS
 var fruitDIV = document.getElementById('fruitDIV');
@@ -54,8 +54,8 @@ function HTMLfruitProduct(con){
                     <p class="card-text">Price: ${FRUIT[con-1].price}.00</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" onclick="cart('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
-                            <button id="${btn}" type="button" onclick="cart2('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
+                            <button type="button" onclick="cart2('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
+                            <button id="${btn}" type="button" onclick="cart('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
                         </div>
                         <small class="text-muted">Free shopping</small>
                     </div>
@@ -83,8 +83,8 @@ function HTMLjuiceProduct(con){
                     <p class="card-text">Price: ${JUICE[con-1].price}.00</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" onclick="cart('${JUICE[con-1].name}','${JUICE[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
-                            <button id="${btn}" type="button" onclick="cart2('${JUICE[con-1].name}','${JUICE[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
+                            <button type="button" onclick="cart2('${JUICE[con-1].name}','${JUICE[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
+                            <button id="${btn}" type="button" onclick="cart('${JUICE[con-1].name}','${JUICE[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
                         </div>
                         <small class="text-muted">Free shopping</small>
                     </div>
@@ -94,9 +94,9 @@ function HTMLjuiceProduct(con){
     `
 }
 
-function HTMLfruitProduct(con){
-    let URL = `img/fruits/fruit${con}.jpg`;
-    let btn = `btnFruit${con}`;
+function HTMLsaladProduct(con){
+    let URL = `img/salads/salad${con}.jpg`;
+    let btn = `btnSalad${con}`;
     return `
         <div class="col-md-4">
             <div class="card_n mb-4 shadow-sm">
@@ -108,12 +108,12 @@ function HTMLfruitProduct(con){
                     <i style="color:orange;" class="fa fa-star" ></i>
                     <i style="color:orange;" class="fa fa-star" ></i>
                     <i style="color:orange;" class="fa fa-star" ></i>
-                    <p class="card-text">${FRUIT[con-1].name}</p>
-                    <p class="card-text">Price: ${FRUIT[con-1].price}.00</p>
+                    <p class="card-text">${SALAD[con-1].name}</p>
+                    <p class="card-text">Price: ${SALAD[con-1].price}.00</p>
                     <div class="d-flex justify-content-between align-items-center">
                         <div class="btn-group">
-                            <button type="button" onclick="cart('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
-                            <button id="${btn}" type="button" onclick="cart2('${FRUIT[con-1].name}','${FRUIT[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
+                            <button type="button" onclick="cart2('${SALAD[con-1].name}','${SALAD[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Buy</button>
+                            <button id="${btn}" type="button" onclick="cart('${SALAD[con-1].name}','${SALAD[con-1].price}','${URL}','${con}','${btn}')" class="btn btn-sm btn-outline-secondary">Add to cart</button>
                         </div>
                         <small class="text-muted">Free shopping</small>
                     </div>
@@ -122,7 +122,38 @@ function HTMLfruitProduct(con){
         </div>
     `
 }
-
+//ANIMATION
+function animation(){
+    const toast=swal.mixin({
+        toast:true,
+        position:'top-end',
+        showConfirmButton:false,
+        timer:1000
+    });
+    toast({
+        type: 'success',
+        title: 'Added to shopping cart'
+    });
+}
+//CART FUNCTIONS
+function cart(name, price, url, con, btncart){
+    var item={
+        name:name,
+        price:price,
+        url:url
+    }
+    cartItems.push(item);
+    let storage = JSON.parse(localStorage.getItem("cart"));
+    if (storage==null) {
+            products.push(item);
+            localStorage.setItem("cart", JSON.stringify(products));
+    } else {
+        products = JSON.parse(localStorage.getItem("cart"));
+        cart_n.innerHTML = `[${products.length}]`;
+        document.getElementById(btncart).style.display="none";
+        animation();
+    }
+}
 //RENDER
 function render(){
     for (let index = 1; index <= 6; index++){
@@ -130,5 +161,12 @@ function render(){
     }
     for (let index = 1; index <= 3; index++){
         juiceDIV.innerHTML += `${HTMLjuiceProduct(index)}`;
+        saladDIV.innerHTML += `${HTMLsaladProduct(index)}`;
+    }
+    if(localStorage.getItem("cart")==null){
+
+    } else{
+        products = JSON.parse(localStorage.getItem("cart"));
+        cart_n.innerHTML=`[${products.length}]`;
     }
 }
